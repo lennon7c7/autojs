8540b6e034b22e12b708a47197edc453ddf24e37
lennon <bawgkh@qq.com>
2020-09-13 01:44:29 +0800

    feat: 通过验证码

--- kuaishou-money.js
+++ kuaishou-money.js
@@ -1,7 +1,7 @@
 /**
  * 快手-所有金币任务
  * 当前存在问题
- * 1. 无法判断是否处于验证码状态
+ * 1. 无法判断是否成功通过验证码（就随便滑动，瞎蒙呗）
  */
 var clicks = require('function-clicks.js');
 var others = require('function-others.js');
@@ -20,16 +20,16 @@ function main() {
     if (!status) {
         return false;
     }
-    
+
     if (!clicks.id('redFloat')) {
         return false;
     }
 
     status1 = taskTreasureBox();
-    status2 = taskAd10();
-    status3 = taskVideo();
+    // status2 = taskAd10();
+    // status3 = taskVideo();
 
-    if (status1 && status2 && status3) {
+    if (status1) {
         others.exitApp(PACKAGE_NAME);
         exit();
     }
@@ -43,7 +43,7 @@ function taskTreasureBox() {
     console.log("---------- taskTreasureBox start ----------");
 
     if (!clicks.text("开宝箱得金币")) {
-        return false;
+        return true;
     }
 
     swipes.return();
@@ -56,7 +56,7 @@ function taskTreasureBox() {
 // 任务-10次广告
 function taskAd10() {
     console.log("---------- taskAd10 start ----------");
-  
+
     if (text("明日再来").exists()) {
         return true;
     }
@@ -91,6 +91,12 @@ function taskVideo() {
     swipes.return();
 
     for (var i = 0; i < 1200; i++) {
+        if (text('Drag the slider').exists()) {
+            swipe(87, 969, 700, 969, 700);
+            sleeps.s2to3();
+            swipes.return();
+        }
+
         swipes.down1600();
         sleeps.s10to30();
     }
