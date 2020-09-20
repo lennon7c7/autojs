ea22b653aeadb003c688e21dba44079aaf9ab2e2
lennon <bawgkh@qq.com>
2020-09-20 20:09:06 +0800

    fix: 兼容性

--- wesee.js
+++ wesee.js
@@ -14,14 +14,14 @@ while (true) {
 function main() {
     others.initEnv();
 
-    var status = others.launchApp(PACKAGE_NAME);
+    status = others.launchApp(PACKAGE_NAME);
     if (!status) {
         return false;
     }
 
-    status1 = taskVideo();
+    status = taskVideo();
 
-    if (status1) {
+    if (status) {
         others.exit();
     }
 }
@@ -32,8 +32,10 @@ function taskVideo() {
 
     clicks.xy(10, 2200);
 
-    for (var i = 0; i < 3; i++) {
-        clicks.text('取消');
+    for (var i = 0; i < 2; i++) {
+        if (text('取消').exists()) {
+            clicks.text('取消');
+        }
 
         swipes.down1600();
         sleeps.s5to10();
@@ -42,9 +44,40 @@ function taskVideo() {
     clicks.xy(888, 2200);
     clicks.xy(750, 411);
     sleeps.s2to3();
+  
     if (text('明日再来领现金').exists()) {
-        return true
+        return true;
     }
+    
+    if (clicks.text('签到领红包')) {
+        others.back();
+        clicks.xy(750, 411);
+    }
+    
+    if (clicks.text('领取 x1')) {
+        others.back();
+        clicks.xy(750, 411);
+    }
+    
+    if (clicks.text('领取 x2')) {
+        others.back();
+        clicks.xy(750, 411);
+    }
+    
+    if (clicks.text('领取 x3')) {
+        others.back();
+        clicks.xy(750, 411);
+    }
+    
+    if (clicks.text('领取 x6')) {
+        others.back();
+        clicks.xy(750, 411);
+    }
+   
+    if (text('明日再来领现金').exists()) {
+        return true;
+    }
+   
     others.back();
     clicks.xy(10, 2200);
 
