2753a10eab323fa767a9314c12a0483e23d3cb03
lennon <lennonandjune@gmail.com>
2020-12-24 00:43:57 +0800

    feat: taskCashout

--- module/momo.js
+++ module/momo.js
@@ -14,7 +14,7 @@ s.PACKAGE_NAME = 'com.immomo.young';
  * 任务-签到
  */
 function taskCheckin() {
-    log('----------', s.PACKAGE_NAME, 'taskCashout start ----------');
+    log('----------', s.PACKAGE_NAME, 'taskCheckin start ----------');
 
     if (text('稍后更新').exists()) {
         clicks.centerXyByText('稍后更新');
@@ -45,16 +45,55 @@ function taskCheckin() {
 }
 
 /**
+ * 任务-提现
+ */
+function taskCashout() {
+    log('----------', s.PACKAGE_NAME, 'taskCashout start ----------');
+
+    if (!others.backToElement(text('签到领现金'))) {
+        return false;
+    }
+
+    if (!text('已经连续签到21天').exists()) {
+        return false;
+    }
+
+    swipes.down();
+    if (!clicks.centerXyByText('累计活动收益')) {
+        return false;
+    }
+
+    if (text('0.0元').exists() && !text('确认提现').exists()) {
+        return true;
+    }
+
+    if (!clicks.text('确认提现')) {
+        return false;
+    }
+
+    if (!clicks.text('确定')) {
+        return false;
+    }
+
+    if (!clicks.text('知道了')) {
+        return false;
+    }
+
+    return true;
+}
+
+/**
  * 入口-开始调用
  * @returns {boolean}
  */
 s.start = function () {
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 6; i++) {
         others.launch(s.PACKAGE_NAME);
 
-        status = taskCheckin();
+        status0 = taskCheckin();
+        status1 = taskCashout();
 
-        if (status) {
+        if (status0 && status1) {
             return true;
         }
     }
