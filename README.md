69bed6ce90bb0a784accb9d0e0c3a330b09941b4
lennon <lennonandjune@gmail.com>
2021-03-27 16:48:44 +0800

    feat: initFromLocalPC

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # autojs
 一些autojs的脚本
+一开始是帮老婆刷淘宝金币、考拉豆用的，再后面也找点其它应用去练手玩玩
+代码多了之后，就索性开了个仓库维护
 
-参考文档：
-https://hyb1996.github.io/AutoJs-Docs
+
+## 功能特点
+1. 区分模块
+2. 集成常用功能
+
+
+## 初始化
+文件位于 initFromLocalPC.js
+
+
+## 参考文档
+AutoJs-Docs：https://hyb1996.github.io/AutoJs-Docs
+
+
+## FAQ
+Q：这玩意赚钱不？
+A：赚钱是不可能的，想啥呢你
---
+++ initFromLocalPC.js
@@ -0,0 +1,110 @@
+/**
+ * 初始化代码
+ * 从 PC端 到 手机端
+ */
+main();
+
+/**
+ * 入口
+ */
+function main() {
+    toastLog('---------- start ----------');
+
+    // step1: 手机连接PC
+
+    // step2: 手动开启文件服务，下面是php的例子
+    // php -S 192.168.25.222:9999
+
+    // step3: 配置同步的文件
+    var fileList = configHttp();
+
+    // step4: 获取文件
+    var codeList = httpGet(fileList);
+
+    // step5: 写入手机端，会强行覆盖文件
+    var isOk = overWriteFile(codeList);
+    if (!isOk) {
+        toastLog('shit happen');
+    }
+
+    toastLog('---------- end ----------');
+}
+
+/**
+ * 配置同步的文件
+ * @returns {array} 代码内容列表
+ */
+function configHttp() {
+    var returnList = [];
+
+    domain = 'http://192.168.25.222:9999/';
+    filenameList = [
+        'function/clicks.js', 'function/exists.js', 'function/others.js', 'function/sleeps.js', 'function/swipes.js',
+
+        'module/baidu.js', 'module/baiduhaokan.js', 'module/baidutieba.js',
+        'module/diandiancaige.js', 'module/douyin.js', 'module/douyinlite.js', 'module/douyinhuoshan.js',
+        'module/fanqie.js', 'module/fanqiechangting.js',
+        'module/huohuoshipin.js', 'module/huoshan.js',
+        'module/jibubao.js', 'module/jingdonglite.js', 'module/jukandian.js',
+        'module/kaola.js', 'module/kuaishou.js', 'module/kuaiyin.js', 'module/kugoudaziban.js',
+        'module/mojitianqi.js', 'module/momo.js',
+        'module/oupenglite.js', 'module/pinduoduo.js',
+        'module/qqbrowser.js', 'module/qqreader.js', 'module/qutoutiao.js', 'module/shuqi.js', 'module/tianmao.js',
+        'module/taobao.js', 'module/taobaolive.js', 'module/tencentnews.js', 'module/tencentnow.js', 'module/toutiao.js',
+        'module/uc.js',
+        'module/weishi.js', 'module/weixin.js', 'module/ximalaya.js', 'module/zhifubao.js', 'module/zhongqingkandian.js', 'module/zuiqiangdaren.js',
+
+        'action-all.js', 'action-every-20-min.js', 'test-swipe.js',
+    ];
+
+    filenameList.forEach((value, key) => {
+        returnList[key] = {
+            'url': domain + value,
+            'path': files.cwd() + '/' + value,
+        }
+    });
+
+    return returnList;
+}
+
+/**
+ * 获取文件
+ * @returns {array} 代码内容列表
+ */
+function httpGet(fileList) {
+    var returnList = [];
+
+    fileList.forEach((value, key) => {
+        var content = http.get(value.url).body.string();
+        if (!content) {
+            toastLog('!content');
+            return;
+        }
+
+        returnList[key] = {
+            'path': value.path,
+            'content': content,
+        }
+    });
+
+    return returnList;
+}
+
+/**
+ * 写入手机端，会强行覆盖文件
+ */
+function overWriteFile(codeList) {
+    var isOk = false;
+
+    codeList.forEach((value) => {
+        isOk = files.ensureDir(value.path);
+        if (!isOk) {
+            toastLog('!isOk');
+            return;
+        }
+
+        files.write(value.path, value.content);
+    });
+
+    return isOk;
+}
