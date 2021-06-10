58f8aa685c158d43cf860e15810628e5085ce709
lennon <lennonandjune@gmail.com>
2021-06-11 00:10:56 +0800

    fix: taskMomentLike 英文转中文

--- module/weixin.js
+++ module/weixin.js
@@ -1,6 +1,5 @@
 /**
- * 微信-任务
- * 我的系统语言是英文，所以需要的自己替换成中文
+ * 微信
  */
 var clicks = require('../function/clicks.js');
 var exists = require('../function/exists.js');
@@ -16,16 +15,38 @@ currentAPP.NAME = getAppName(currentAPP.PACKAGE_NAME);
  * 任务-朋友圈-点赞
  */
 function taskMomentLike() {
-    if (!clicks.centerXyByText('Discover')) {
+    // 点赞当前页面的动态
+    function clickLikeButton() {
+        // 这里是需找到可以滚动的控件：ListView，className是不会改变的，这样写出来的程序会比较稳定
+        var scroll = className('ListView').findOne();
+        // 子控件
+        var scroll_thing = scroll.children();
+        // 遍历子控件
+        scroll_thing.forEach((item, position) => {
+            var comment = item.findOne(desc('评论'));
+            if (comment) {
+                comment.click();
+                sleep(200);
+
+                // 根据当前的选择器所确定的筛选条件，对屏幕上的控件进行搜索，如果找到符合条件的控件则返回该控件；否则返回null
+                clicks.centerXyByText('赞');
+            }
+        });
+
+        // 直接调用向下滚动的方法
+        scrollDown();
+        sleeps.s2to5();
+    }
+
+    if (!clicks.backToElement(text('发现'))) {
         return false;
     }
 
-    if (!clicks.centerXyByText('Moments')) {
+    if (!clicks.backToElement(text('朋友圈'))) {
         return false;
     }
 
-    if (!text('Tap to change album cover').exists()) {
-        toast('双击 顶部-更新朋友圈');
+    if (!text('轻触更换主题照片').exists()) {
         click(108, 136);
         sleep(100);
         clicks.xy(108, 136)
@@ -36,29 +57,6 @@ function taskMomentLike() {
     }
 
     return true;
-}
-
-// 点赞当前页面的动态
-function clickLikeButton() {
-    // 这里是需找到可以滚动的控件：ListView，className是不会改变的，这样写出来的程序会比较稳定
-    var scroll = className('ListView').findOne();
-    // 子控件
-    var scroll_thing = scroll.children();
-    // 遍历子控件
-    scroll_thing.forEach((item, position) => {
-        var comment = item.findOne(desc('Comment'));
-        if (comment) {
-            comment.click();
-            sleep(200);
-
-            // 根据当前的选择器所确定的筛选条件，对屏幕上的控件进行搜索，如果找到符合条件的控件则返回该控件；否则返回null
-            clicks.centerXyByText('Like');
-        }
-    });
-
-    // 直接调用向下滚动的方法
-    scrollDown();
-    sleeps.s2to5();
 }
 
 /**
@@ -523,8 +521,8 @@ currentAPP.start = function () {
 
 
         // status0 = taskReadBiKanYanXuan();
-        // status1 = taskMomentLike();
-        taskMP();
+        taskMomentLike();
+        // taskMP();
 
         if (status0) {
             return true;
