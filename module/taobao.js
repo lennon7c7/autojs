d869728d24dfaae53ceb54f474b60ca39cbe7a61
lennon <lennonandjune@gmail.com>
2022-05-28 22:27:00 +0800

    fix: taobao

--- module/taobao.js
+++ module/taobao.js
@@ -14,17 +14,166 @@ currentAPP.VERSION = ''
 currentAPP.APK = ''
 
 
+// 任务-收金币
+function taskShouJinBi() {
+    // 淘宝 - 金币小镇
+    MP_URL = 'tbopen://m.taobao.com/tbopen/index.html?h5Url=https%3A%2F%2Fpages.tmall.com%2Fwow%2Fz%2Ftmtjb%2Ftown%2Fhome%3Fwh_biz%3Dtm%26disableNav%3DYES%26hd_from%3Dalipayjf%26remind%3Dtrue&action=ali.open.nav&module=h5&bootImage=0&slk_sid=rndb5fbc2_1641359630456&slk_t=1641359630900&afcPromotionOpen=false&source=slk_dp'
+    app.startActivity({ data: MP_URL })
+    sleeps.s10()
+
+    if (!className('android.widget.Button').depth(13).indexInParent(0).findOne(3000)) {
+        return false
+    }
+    className('android.widget.Button').depth(13).indexInParent(0).findOne(3000).click()
+    sleeps.s3()
+
+    clicks.textIfExists('收金币')
+    clicks.textIfExists('助力Ta')
+    others.back()
+}
+
+// 任务-赚金币
+function taskZhuanJinBi() {
+     // 淘宝 - 金币小镇
+     MP_URL = 'tbopen://m.taobao.com/tbopen/index.html?h5Url=https%3A%2F%2Fpages.tmall.com%2Fwow%2Fz%2Ftmtjb%2Ftown%2Fhome%3Fwh_biz%3Dtm%26disableNav%3DYES%26hd_from%3Dalipayjf%26remind%3Dtrue&action=ali.open.nav&module=h5&bootImage=0&slk_sid=rndb5fbc2_1641359630456&slk_t=1641359630900&afcPromotionOpen=false&source=slk_dp'
+     app.startActivity({ data: MP_URL })
+     sleeps.s10()
+ 
+    if (!clicks.element(className('android.widget.Button').depth(11).indexInParent(3))) {
+        return false
+    }
+
+    for (var i = 0; i < 3; i++) {
+        clicks.textIfExists('领取奖励')
+    }
+
+    clicks.textIfExists('去签到')
+
+    if (clicks.textIfExists('找答案')) {
+        className('android.widget.Button').depth(7).indexInParent(7).findOne(3000).click()
+        sleeps.s3()
+
+        exists.backToElement(text('今日任务'))
+        clicks.textIfExists('领取奖励')
+    }
+
+    if (clicks.textIfExists('去蚂蚁庄园逛一逛(0/1)')) {
+        sleeps.s10()
+
+        exists.backToElement(text('今日任务'))
+        clicks.textIfExists('领取奖励')
+    }
+
+    if (clicks.textIfExists('去蚂蚁森林收能量(0/1)')) {
+        sleeps.s10()
+
+        exists.backToElement(text('今日任务'))
+        clicks.textIfExists('领取奖励')
+    }
+
+    for (var i = 0; i < 3; i++) {
+        if (clicks.textIfExists('去逛逛')) {
+
+            for (var k = 0; k < 15; k++) {
+                swipes.down()
+                sleeps.s1()
+            }
+
+            exists.backToElement(text('今日任务'))
+            clicks.textIfExists('领取奖励')
+        }
+    }
+
+    for (var i = 0; i < 3; i++) {
+        if (clicks.textIfExists('逛一下')) {
+
+            for (var k = 0; k < 15; k++) {
+                swipes.down()
+                sleeps.s1()
+            }
+
+            exists.backToElement(text('今日任务'))
+            clicks.textIfExists('领取奖励')
+        }
+    }
+
+    if (clicks.textIfExists('进直播间看看(0/1)')) {
+        sleeps.s30to35()
+
+        exists.backToElement(text('今日任务'))
+        clicks.textIfExists('领取奖励')
+    }
+
+    if (clicks.textIfExists('看好友动态得金币(0/1)')) {
+        sleeps.s15to20()
+
+        exists.backToElement(text('今日任务'))
+        clicks.textIfExists('领取奖励')
+    }
+
+    if (clicks.textIfExists('去看淘金币省了多少钱(0/1)')) {
+        exists.backToElement(text('今日任务'))
+        clicks.textIfExists('领取奖励')
+    }
+
+    if (clicks.textIfExists('金币游乐园玩赚淘金币(0/1)')) {
+        sleeps.s15to20()
+
+        exists.backToElement(text('今日任务'))
+        clicks.textIfExists('领取奖励')
+    }
+
+    if (clicks.textIfExists('搜一下')) {
+        clicks.xy(device.width / 2 - 200, device.height / 2)
+        sleeps.s15to20()
+        exists.backToElement(text('今日任务'))
+        clicks.textIfExists('领取奖励')
+    }
+
+    if (clicks.textIfExists('好店浏览得奖励(0/1)')) {
+        sleeps.s15to20()
+        exists.backToElement(text('今日任务'))
+        clicks.textIfExists('领取奖励')
+    }
+
+    if (clicks.textIfExists('福气兑无门槛红包(0/1)')) {
+        clicks.xy(device.width / 2, device.height / 2)
+        sleeps.s30to35()
+        others.back4()
+        back()
+        back()
+        sleeps.s3()
+        exists.backToElement(text('今日任务'))
+        clicks.textIfExists('领取奖励')
+    }
+}
+
 /**
  * 成就中心
  */
 function taskLife() {
-    MP_URL = 'tbopen://m.taobao.com/tbopen/index.html?h5Url=https://m.tb.cn/h.fqgCv8Z?sm=dcc4ef'
+    MP_URL = 'tbopen://m.taobao.com/tbopen/index.html?h5Url=https://pages.tmall.com/wow/z/tblife/achievement/nfcFHNzbzbAcJ8CBm3Am?disableNav=YES&from=share&sourceType=other&suid=d25bf0e4-aeda-4b2f-b0e6-0c68b94ed547&ut_sk=1.XXeQ2K0gRm8DAD%2FUgoGHfv2o_21646297_1652493645168.DingTalk.TBLife-1&un=2bf412b10649e7d5bbeeeafdb2768e28&share_crt_v=1&un_site=0&sp_abtk=common_TBLife-1_commonInfo&cpp=1&shareurl=true&spm=a313p.22.ce8hnk.1392565422159&short_name=h.fIX1X0P&app=chrome#/'
     app.startActivity({ data: MP_URL })
     sleeps.s20()
 
     clicks.element(textEndsWith('/100成就点'))
     sleeps.s3()
 
+
+    textString = '淘金币 +64';
+    if (text(textString).exists()) {
+        text(textString).findOnce().parent().children().forEach(function (child) {
+            if (child.text() == '选TA') {
+                child.click()
+                sleeps.s3()
+                clicks.textIfExists('继续去集成就点')
+
+                clicks.element(textEndsWith('/100成就点'))
+                sleeps.s3()
+                return false
+            }
+        })
+    }
 
     clicks.textIfExists('领取奖励')
     clicks.textIfExists('领取奖励')
@@ -70,10 +219,19 @@ function taskLife() {
         clicks.xy(device.width / 2, device.height - 800)
     }
 
+    if (text('收金币').exists()) {
+        clicks.text('收金币')
+        sleeps.s10()
+
+        clicks.centerXyByText('今日签到')
+
+        others.back()
+        clicks.textIfExists('领取奖励')
+    }
 
     if (text('去下单').exists()) {
         // 淘宝 - 自动下0元单
-        MP_URL = 'tbopen://m.taobao.com/tbopen/index.html?h5Url=https://m.tb.cn/h.fLsfsEG?sm=0ad556'
+        MP_URL = 'tbopen://m.taobao.com/tbopen/index.html?h5Url=https%3A%2F%2Fmarket.m.taobao.com%2Fapp%2Fminiapp-biz%2Fqrcode%2Findex.html%3F_ariver_appid%3D3000000059554605%26ad_type%3D1.0%26x_object_type%3Dminiapp%26adScene%3Dxxl-news-pop%26hd_ad_includeIds%3D126087874%26sp_abtk%3Dcommon_1115_miniapp_commonInfo%26suid%3D5e757595-ddb2-47a9-b941-35444b33f7ee%26prismChannel%3Dpoplayer%26adTrace%3D272326504420001__user_define__21207b9016482542051316081e563f__I__C__0%26_mp_code%3Dtb%26bxsign%3Dscdk8OFIxRqfHtCG16Dbju-GH169kzt7kD7etPRCIReNfLmkxJJYTMqSZPqosS-JXhbgIRU-ZnPZH7h3e0-Evvsmmiw_U0Hn5qEU0rTxU3Uekpx0wUw58YZnJRK_nNa-YG6%26un%3D2bf412b10649e7d5bbeeeafdb2768e28%26shareurl%3Dtrue%26share_crt_v%3D1%26ut_sk%3D1.XXeQ2K0gRm8DAD%252FUgoGHfv2o_21646297_1652107397885.DingTalk.1115_miniapp%26app%3Dchrome%26cpp%3D1%26settleTime%3D0%26epid%3Dmm_12852562_1778064_110736900105%26clickid%3DA17_24435877416482542137673135%26transition%3Dpresent%26shortTfs%3D149dff41288eea4dfcc913cc7c687c23%26spm%3D0.0.c1.d1%26ffs%3Dhttps%253A%252F%252Fifsk2.tanx.com%252Fffsback%253Fname%253Dminteract%2526pid%253Dmm_12852562_1778064_110736900105%2526pvid%253D4b9e1821d37fe700623e5cfd26139e65%2526biz%253DVfBUCgMKATESEgj9ufmRBjIKbGVubm9uMTk5MRo6CjgKJTI3MjMyNjUwNDQyMDAwMV8zMzAwMTg5MDYyXzMzMDMxMzk3NzIYwuWPPCoBSTIBQ5ABwuWPPA__%26un_site%3D0%26sourceType%3DminiApp%26x_miniapp_id%3D3000000059554605%26short_name%3Dh.fr9fiA3%26page%3Dpages%252Findex%252Findex%26settleType%3DafterTime'
         app.startActivity({ data: MP_URL })
         sleeps.s10()
 
@@ -94,14 +252,10 @@ function taskLife() {
 function taskShop() {
     log('----------', currentAPP.NAME, arguments.callee.name, 'start ----------')
 
-    if (!others.backToElement(desc('我的淘宝'))) {
-        return false
-    }
-
-    if (!clicks.centerXyByDesc('淘金币') && !clicks.centerXyByDesc('领淘金币')) {
-        return false
-    }
-    sleeps.s15()
+    // 淘宝 - 金币小镇
+    MP_URL = 'tbopen://m.taobao.com/tbopen/index.html?h5Url=https%3A%2F%2Fpages.tmall.com%2Fwow%2Fz%2Ftmtjb%2Ftown%2Fhome%3Fwh_biz%3Dtm%26disableNav%3DYES%26hd_from%3Dalipayjf%26remind%3Dtrue&action=ali.open.nav&module=h5&bootImage=0&slk_sid=rndb5fbc2_1641359630456&slk_t=1641359630900&afcPromotionOpen=false&source=slk_dp'
+    app.startActivity({ data: MP_URL })
+    sleeps.s10()
 
     if (!className('android.widget.Button').depth(12).indexInParent(0).findOne(3000)) {
         return false
@@ -120,6 +274,11 @@ function taskShop() {
         if (text('关注+10').exists()) {
             clicks.centerXyByText('关注+10')
         }
+
+        if (text('立即领5淘金币').exists()) {
+            clicks.textIfExists('立即领5淘金币')
+            exists.backToElement(text('浏览送金币'))
+        }
     })
 
     if (desc('已完成').find().size() > 6) {
@@ -133,14 +292,10 @@ function taskShop() {
 function taskHelpFriend() {
     log('----------', currentAPP.NAME, arguments.callee.name, 'start ----------')
 
-    if (!others.backToElement(desc('我的淘宝'))) {
-        return false
-    }
-
-    if (!clicks.centerXyByDesc('淘金币') && !clicks.centerXyByDesc('领淘金币')) {
-        return false
-    }
-    sleeps.s15()
+    // 淘宝 - 金币小镇
+    MP_URL = 'tbopen://m.taobao.com/tbopen/index.html?h5Url=https%3A%2F%2Fpages.tmall.com%2Fwow%2Fz%2Ftmtjb%2Ftown%2Fhome%3Fwh_biz%3Dtm%26disableNav%3DYES%26hd_from%3Dalipayjf%26remind%3Dtrue&action=ali.open.nav&module=h5&bootImage=0&slk_sid=rndb5fbc2_1641359630456&slk_t=1641359630900&afcPromotionOpen=false&source=slk_dp'
+    app.startActivity({ data: MP_URL })
+    sleeps.s10()
 
     clicks.textIfExists('签到领金币')
     clicks.textIfExists('我知道了')
@@ -194,7 +349,12 @@ function taskHelpFriend() {
 function taskMoneyPower() {
     log('----------', currentAPP.NAME, arguments.callee.name, 'start ----------')
 
-    if (!others.backToElement(text('赚金币'))) {
+    // 淘宝 - 金币小镇
+    MP_URL = 'tbopen://m.taobao.com/tbopen/index.html?h5Url=https%3A%2F%2Fpages.tmall.com%2Fwow%2Fz%2Ftmtjb%2Ftown%2Fhome%3Fwh_biz%3Dtm%26disableNav%3DYES%26hd_from%3Dalipayjf%26remind%3Dtrue&action=ali.open.nav&module=h5&bootImage=0&slk_sid=rndb5fbc2_1641359630456&slk_t=1641359630900&afcPromotionOpen=false&source=slk_dp'
+    app.startActivity({ data: MP_URL })
+    sleeps.s10()
+
+    if (!clicks.element(className('android.widget.Button').depth(11).indexInParent(3))) {
         return false
     }
 
@@ -358,6 +518,14 @@ function taskMoneyPower() {
         clicks.centerXyByText('领取奖励')
     }
 
+    if (clicks.textIfExists('去蚂蚁庄园捐蛋(0/1)')) {
+        others.back2()
+        if (!text('今日任务').exists()) {
+            return false
+        }
+        clicks.centerXyByText('领取奖励')
+    }
+
     // 禁用原因：因为多个淘宝账号不一定对应上天猫账号，所以还是禁用掉吧
     // if (clicks.textIfExists('去天猫APP领红包(0/1)')) {
     //     sleeps.s10()
@@ -508,11 +676,11 @@ function taskCancelShop() {
         return false
     }
 
-    for (var i = 0; i < 100; i++) {
-        desc('个关注店铺的宝贝上新').exists() && click(986, 565)
+    for (var i = 0; i < 200; i++) {
+        click(986, 373)
         sleep(500)
-        desc('个关注店铺的宝贝上新').exists() && click(986, 565)
-        sleeps.s2to3()
+        click(986, 373)
+        sleep(500)
     }
 
     return true
@@ -563,6 +731,9 @@ currentAPP.start = function () {
         }
 
 
+        taskShop()
+        taskShouJinBi()
+        taskZhuanJinBi()
         status0 = taskLife()
 
         if (status0) {
