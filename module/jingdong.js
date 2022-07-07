452ce4bb8001a996c73d34165bd70b849cfdfa54
lennon <lennonandjune@gmail.com>
2022-07-07 22:37:58 +0800

    feat: jd

--- module/jingdong.js
+++ module/jingdong.js
@@ -13,182 +13,91 @@ currentAPP.NAME = getAppName(currentAPP.PACKAGE_NAME)
 currentAPP.VERSION = '9.4.6'
 currentAPP.APK = 'https://android-apps.pp.cn/fs08/2021/04/06/7/120_da18100cae7d790437e6bb03be0a99d7.apk'
 
-// 任务-签到
-function taskCheckin() {
-    log('----------', currentAPP.NAME, arguments.callee.name, 'start ----------')
-
-    if (!exists.backToElement(desc('首页'))) {
-        return false
-    }
-
-    if (!clicks.textParent('领京豆')) {
-        return false
-    }
-    sleeps.s5()
-
-    if (text('签到领京豆').exists()) {
-        clicks.centerXyByText('签到领京豆')
-    }
-
-    if (textStartsWith('已连签').exists() && !text('签到领京豆').exists()) {
-        return true
-    }
-
-    return false
-}
-
-// 任务-浏览3个商品
-function taskProduuct3() {
-    log('----------', currentAPP.NAME, 'taskProduuct3 start ----------')
-
-    swipes.scrollDown()
-
-    if (text('浏览3个商品').exists()) {
-        i = 0
-        className('android.widget.TextView').depth(3).find().forEach((value1, key1) => {
-            if (i === 4 ||
-                value1.bounds().centerX() <= 0 || value1.bounds().centerY() <= 0 ||
-                value1.text().indexOf('¥') === -1) {
-                return
-            }
-
-            clicks.element(value1)
-            i++
-            others.back()
-        })
-    }
-
-    if (text('点我领取京豆').exists()) {
-        clicks.centerXyByText('点我领取京豆')
-    }
-
-    if (text('已领取奖励').exists()) {
-        scrollUp()
-        sleeps.s3()
-        return true
-    }
-
-    return false
-}
-
 // 任务-升级赚京豆
 function taskSJZJD() {
     log('----------', currentAPP.NAME, arguments.callee.name, 'start ----------')
 
-    for (var i = 0; i < 1; i++) {
-        isClick = false
-        className('android.view.ViewGroup').depth(2).drawingOrder(9).indexInParent(8).find().forEach((value1, key1) => {
-            if (isClick) {
-                return
-            }
+    others.clear()
+    others.launch(currentAPP.PACKAGE_NAME)
 
-            clicks.element(value1)
-            isClick = true
-        })
-        if (!isClick) {
-            return false
-        }
-
-        sleeps.s2to3()
-
-        exists.backToElement(text('去完成'))
+    if (!clicks.textParent('领京豆')) {
+        return false
     }
+    sleeps.s15()
+
+    clicks.xy(700, 1320)
     sleeps.s5()
 
     for (var i = 0; i < 50; i++) {
         if (text('去完成').exists() && clicks.centerXyByText('去完成')) {
-            sleeps.s2to3()
-            others.back()
+            sleeps.s15to20()
+            others.backToElement(text('做任务再升一级可额外得'))
         }
     }
 
     return true
 }
 
-// 任务-抽京豆
-function taskCJD() {
-    log('----------', currentAPP.NAME, arguments.callee.name, 'start ----------')
-
-    if (!exists.backToElement(desc('首页'))) {
-        return false
+// 金融双签
+function stepCheckinJR() {
+    others.clear()
+    URL_OLD = 'https://m1.jr.jd.com/integrate/signin/index.html'
+    MP_URL = 'openApp.jdMobile://virtual?params=%7B%22category%22%3A%22jump%22%2C%22des%22%3A%22m%22%2C%22sourceValue%22%3A%22babel-act%22%2C%22sourceType%22%3A%22babel%22%2C%22M_sourceFrom%22%3A%22h5auto%22%2C%22msf_type%22%3A%22auto%22%2C%22url%22%3A%22' + encodeURIComponent(URL_OLD) + '%22%7D%0A'
+    app.startActivity({ data: MP_URL })
+    sleeps.s10()
+    if (clicks.textIfExists('去签到')) {
+        sleeps.s10()
+        clicks.textIfExists('签到领金贴')
+        clicks.textIfExists('立即获得')
     }
 
-    if (!clicks.textParent('领京豆')) {
-        return false
+    others.clear()
+    URL_OLD = 'https://m1.jr.jd.com/integrate/signin/index.html'
+    MP_URL = 'openApp.jdMobile://virtual?params=%7B%22category%22%3A%22jump%22%2C%22des%22%3A%22m%22%2C%22sourceValue%22%3A%22babel-act%22%2C%22sourceType%22%3A%22babel%22%2C%22M_sourceFrom%22%3A%22h5auto%22%2C%22msf_type%22%3A%22auto%22%2C%22url%22%3A%22' + encodeURIComponent(URL_OLD) + '%22%7D%0A'
+    app.startActivity({ data: MP_URL })
+    sleeps.s10()
+    if (clicks.textIfExists('去签到')) {
+        sleeps.s10()
+        clicks.centerXyByText('签到领京豆')
     }
 
-    isClick = false
-    className('android.view.ViewGroup').depth(2).drawingOrder(10).indexInParent(9).find().forEach((value1, key1) => {
-        if (isClick) {
-            return
-        }
-
-        clicks.element(value1)
-        isClick = true
-    })
-    if (!isClick) {
-        return false
-    }
-    sleeps.s15()
-
-    if (text('今日还可以抽 0 次哦～').exists() || desc('今日还可以抽 0 次哦～').exists()) {
-        return true
-    }
-
-    clicks.xy(device.width / 2, device.height / 2)
-
-    if (text('今日还可以抽 0 次哦～').exists() || desc('今日还可以抽 0 次哦～').exists()) {
-        return true
-    }
-
-    return false
+    others.clear()
+    URL_OLD = 'https://m1.jr.jd.com/integrate/signin/index.html'
+    MP_URL = 'openApp.jdMobile://virtual?params=%7B%22category%22%3A%22jump%22%2C%22des%22%3A%22m%22%2C%22sourceValue%22%3A%22babel-act%22%2C%22sourceType%22%3A%22babel%22%2C%22M_sourceFrom%22%3A%22h5auto%22%2C%22msf_type%22%3A%22auto%22%2C%22url%22%3A%22' + encodeURIComponent(URL_OLD) + '%22%7D%0A'
+    app.startActivity({ data: MP_URL })
+    sleeps.s10()
+    clicks.textIfExists('完成双签领取')
 }
 
-// 任务-摇京豆
-function taskYJD() {
-    log('----------', currentAPP.NAME, arguments.callee.name, 'start ----------')
+// 京喜双签
+function stepCheckinJX() {
+    others.clear()
+    URL_OLD = 'https://m1.jr.jd.com/integrate/signin/index.html'
+    MP_URL = 'openApp.jdMobile://virtual?params=%7B%22category%22%3A%22jump%22%2C%22des%22%3A%22m%22%2C%22sourceValue%22%3A%22babel-act%22%2C%22sourceType%22%3A%22babel%22%2C%22M_sourceFrom%22%3A%22h5auto%22%2C%22msf_type%22%3A%22auto%22%2C%22url%22%3A%22' + encodeURIComponent(URL_OLD) + '%22%7D%0A'
+    app.startActivity({ data: MP_URL })
+    sleeps.s10()
+    others.back()
+    sleeps.s10()
+}
 
-    if (!exists.backToElement(desc('首页'))) {
-        return false
-    }
+// 抽京豆
+function stepCJD() {
+    others.clear()
+    MP_URL = 'openapp.jdmobile://virtual?params=%7B%22category%22%3A%22jump%22%2C%22des%22%3A%22m%22%2C%22sourceValue%22%3A%22babel-act%22%2C%22sourceType%22%3A%22babel%22%2C%22url%22%3A%22https%3A%2F%2Fpro.m.jd.com%2Fmall%2Factive%2F2xoBJwC5D1Q3okksMUFHcJQhFq8j%2Findex.html%3F_ts%3D1654147266550%26utm_user%3Dplusmember%26ad_od%3Dshare%26utm_source%3Dandroidapp%26utm_medium%3Dappshare%26utm_campaign%3Dt_335139774%26utm_term%3DWxfriends%26_openapp%3D1%22%2C%22M_sourceFrom%22%3A%22h5auto%22%2C%22msf_type%22%3A%22adshare%22%2C%22m_param%22%3A%7B%22YINLIUhuanqi%22%3A%22https%3A%2F%2Fpro.m.jd.com%2Fmall%2Factive%2F2xoBJwC5D1Q3okksMUFHcJQhFq8j%2Findex.html%3F_ts%3D1654147266550%26utm_user%3Dplusmember%26ad_od%3Dshare%26utm_source%3Dandroidapp%26utm_medium%3Dappshare%26utm_campaign%3Dt_335139774%26utm_term%3DWxfriends%22%2C%22toappactive%22%3A%221%22%7D%2C%22SE%22%3A%7B%22mt_subsite%22%3A%22%22%2C%22__jdv%22%3A%22122270672%7Candroidapp%7Ct_335139774%7Cappshare%7CQQfriends%7C1653368936533%22%2C%22unpl%22%3A%22%22%2C%22__jda%22%3A%22122270672.1653368792937365289666.1653368792.1653368792.1653368792.1%22%7D%7D'
+    app.startActivity({ data: MP_URL })
+    sleeps.s10()
+    clicks.xy(300, 550)
+    sleeps.s10()
+}
 
-    if (!clicks.textParent('领京豆')) {
-        return false
-    }
-
-    isClick = false
-    className('android.view.ViewGroup').depth(2).drawingOrder(11).indexInParent(10).find().forEach((value1, key1) => {
-        if (isClick) {
-            return
-        }
-
-        clicks.element(value1)
-        isClick = true
-    })
-    if (!isClick) {
-        return false
-    }
-    sleeps.s15()
-
-    if (text('立即签到 领京豆 ').exists()) {
-        clicks.centerXyByText('立即签到 领京豆 ')
-    }
-    if (desc('立即签到 领京豆').exists()) {
-        clicks.centerXyByDesc('立即签到 领京豆')
-    }
-
-    if (text('去摇盒子').exists()) {
-        clicks.centerXyByText('去摇盒子')
-    }
-
-    if (text('再摇一次').exists()) {
-        clicks.centerXyByText('再摇一次')
-    }
-    if (text('摇一摇 有惊喜剩余摇奖1次').exists()) {
-        clicks.centerXyByText('摇一摇 有惊喜剩余摇奖1次')
-    }
-
-    return true
+// 摇京豆
+function stepYJD() {
+    others.clear()
+    URL_OLD = 'https://spa.jd.com/home?source=JingDou&_t=1639646715&lng=108.311635&lat=22.792222&sid=3a9d76979373f3f73facff7568ea487w&un_area=13_1072_28930_29011&_ts=1654147963642&utm_user=plusmember&ad_od=share&utm_source=androidapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=QRCode'
+    MP_URL = 'openApp.jdMobile://virtual?params=%7B%22category%22%3A%22jump%22%2C%22des%22%3A%22m%22%2C%22sourceValue%22%3A%22babel-act%22%2C%22sourceType%22%3A%22babel%22%2C%22M_sourceFrom%22%3A%22h5auto%22%2C%22msf_type%22%3A%22auto%22%2C%22url%22%3A%22' + encodeURIComponent(URL_OLD) + '%22%7D%0A'
+    app.startActivity({ data: MP_URL })
+    sleeps.s10()
+    clicks.textIfExists('立即签到 领京豆')
 }
 
 /**
@@ -202,24 +111,10 @@ currentAPP.start = function () {
             return true
         }
 
-
-        status0 = taskCheckin()
-        if (status0) {
-            status0 = taskProduuct3()
-        }
-        if (status0) {
-            status0 = taskSJZJD()
-        }
-        if (status0) {
-            status0 = taskCJD()
-        }
-        if (status0) {
-            status0 = taskYJD()
-        }
-
-        if (status0) {
-            return true
-        }
+        stepCheckinJR()
+        stepCJD()
+        stepYJD()
+        taskSJZJD()
 
         others.clear()
     }
